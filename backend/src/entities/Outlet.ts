f19bd8b2812f05458c73bfa310299a3ac4b73ded
Sm Alamin <smalamin@Sms-MacBook-Air.local>
2026-03-15 00:14:50 +0600

    refactor: modify outlet entity for auth

--- backend/src/entities/Outlet.ts
+++ backend/src/entities/Outlet.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import * as bcrypt from "bcrypt";
 import { OutletMenuConfig } from "./OutletMenuConfig";
 import { Sale } from "./Sale";
 
@@ -7,11 +14,25 @@ export class Outlet {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Column({ unique: true, length: 6 })
+  outlet_code!: string;
+
   @Column({ type: "text" })
   name!: string;
 
   @Column({ type: "text" })
   address!: string;
+
+  @Column({ length: 255, nullable: true })
+  password!: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+  }
 
   @OneToMany(() => OutletMenuConfig, (cfg) => cfg.outlet)
   menuConfigs!: OutletMenuConfig[];
