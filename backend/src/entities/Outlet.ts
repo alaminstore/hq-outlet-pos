a0bf6fa1079d3a92beec91006886a39d6244d71f
Sm Alamin <smalamin@Sms-MacBook-Air.local>
2026-03-15 00:59:09 +0600

    refactor: auth system will be work only via outlet_code

--- backend/src/entities/Outlet.ts
+++ backend/src/entities/Outlet.ts
@@ -5,7 +5,7 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
-import * as bcrypt from "bcrypt";
+
 import { OutletMenuConfig } from "./OutletMenuConfig";
 import { Sale } from "./Sale";
 
@@ -14,25 +14,14 @@ export class Outlet {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ unique: true, length: 6 })
-  outlet_code!: string;
+  @Column({ name: "outlet_code", unique: true, length: 6 })
+  outletCode!: string;
 
   @Column({ type: "text" })
   name!: string;
 
   @Column({ type: "text" })
   address!: string;
-
-  @Column({ length: 255, nullable: true })
-  password!: string;
-
-  @BeforeInsert()
-  async hashPassword() {
-    if (this.password) {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-    }
-  }
 
   @OneToMany(() => OutletMenuConfig, (cfg) => cfg.outlet)
   menuConfigs!: OutletMenuConfig[];
